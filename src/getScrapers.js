db747ce01d2b156a81eb13bf480223565152d601
Michael Leaney <mleaney@tokbox.com>
2016-11-30 12:07:15 +1100

    add debug messages, fix logs, better errors

--- src/getScrapers.js
+++ src/getScrapers.js
@@ -5,7 +5,10 @@ import filesParser from './lib/filesParser.js';
 
 const ONE_HOUR = 3600000;
 
-const initializeScraper = (scraper) => {
+const initializeScraper = ([scraper, file]) => {
+  if (typeof scraper !== 'function') {
+    throw new Error(`Scraper must be a factory function that returns a scraper definition. See ${file}`);
+  }
   const scraperInstance = scraper();
 
   scraperInstance.construct = scraper;
@@ -22,9 +25,11 @@ const initializeScraper = (scraper) => {
   }
 
   /* eslint-disable no-param-reassign, no-console */
-  scraperInstance.log = debug(`scraperjs ${scraper.name}:log`);
-  scraperInstance.warn = debug(`scraperjs ${scraper.name}:warn`);
-  scraperInstance.error = debug(`scraperjs ${scraper.name}:error`);
+  scraperInstance.log = debug(`scraperjs:log:${scraperInstance.name}`);
+  scraperInstance.warn = debug(`scraperjs:warn:${scraperInstance.name}`);
+  scraperInstance.error = debug(`scraperjs:error:${scraperInstance.name}`);
+  scraperInstance.debug = debug(`scraperjs:debug:${scraperInstance.name}`);
+
   /* eslint-enable no-param-reassign, no-console */
 
   return scraperInstance;
@@ -32,7 +37,7 @@ const initializeScraper = (scraper) => {
 
 export default function getScrapers(files) {
   return filesParser(files)
-    .then(_.map(file => require(file))) // eslint-disable-line global-require
-    .then(_.map(scraper => scraper.default || scraper))
+  .then(_.map(file => [require(file), file])) // eslint-disable-line global-require
+    .then(_.map(([scraper, file]) => [scraper.default || scraper, file]))
     .then(_.map(initializeScraper));
 }
