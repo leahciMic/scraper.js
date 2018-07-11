92273b85522acdcf5c09938b7659e55be6b6063f
Michael Leaney <leahcimic@gmail.com>
2018-07-11 21:57:06 +0800

    Change initializeScraper input to constructor
    This is easier to work with than a path.

--- src/getScrapers.js
+++ src/getScrapers.js
@@ -3,5 +3,8 @@ const initializeScraper = require('./lib/initializeScraper');
 
 module.exports = async function getScrapers(files) {
   const scrapers = await filesParser(files);
-  return scrapers.map(initializeScraper);
+  return scrapers
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    .map(path => require(path))
+    .map(initializeScraper);
 };
