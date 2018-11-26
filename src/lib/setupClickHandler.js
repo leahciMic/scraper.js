5b40480722ce9fd058ff355880a734cf991e0515
Michael Leaney <leahcimic@gmail.com>
2018-11-26 11:05:37 +0800

    Remove bug when calling .click() on empty $ set

--- src/lib/setupClickHandler.js
+++ src/lib/setupClickHandler.js
@@ -1,7 +1,10 @@
 module.exports = async function setupClickHandler(browser) {
   await browser.evaluate(() => {
     window.$scraperJS.fn.click = function click() {
-      this.get(0).click();
+      if (this.get(0)) {
+        this.get(0).click();
+      }
+      console.warn('No item to click');
     };
   });
 };
