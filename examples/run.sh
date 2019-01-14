a114417046680f951c7e9b44a0dc9cdac3d9a346
Michael Leaney <leahcimic@gmail.com>
2019-01-14 21:08:49 +0800

    Clean old non-working examples(new ones coming soon)

--- examples/run.sh
+++ examples/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-redis-cli FLUSHALL
-HEADLESS=false DEBUG=scraper* node ../src/run.js --concurrency 2 --queue scraper.js-queue-bull --data data.js ./reddit.js
+
+# redis-cli FLUSHALL
+# HEADLESS=false DEBUG=scraper* node ../src/run.js --concurrency 2 --queue scraper.js-queue-bull --data data.js ./reddit.js
--- examples/test.js
+++
@@ -1,1 +0,0 @@
-const a = require('scraper.js-queue-bull');
